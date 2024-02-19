59571e86615caa4caa0d55d7d1558e6aa9253b93
Marcia <hmarcia40@gmail.com>
2024-02-19 11:19:19 -0500

    remove 'magic numebers'

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -21,7 +21,9 @@ const Motivation = [
 export default function Card({ task }) {
   const [newTime, setTime] = useState('')
   const [newDate, setDate] = useState('')
-  const uniqueKey = useRef(Math.random().toString(36).substring(7))
+  const maxCharacters = 36;
+  const maxSubString = 7;
+  const uniqueKey = useRef(Math.random().toString(maxCharacters).substring(maxSubString))
   const { data, error, isLoading } = useSWR(`https://api.thecatapi.com/v1/images/search?key=${uniqueKey.current}`, fetcher)
   if (isLoading) {
     return <div>Loading data...</div>
@@ -29,9 +31,13 @@ export default function Card({ task }) {
   if (error) {
     return <div>There was an error!</div>
   }
+  // function for the time button, prompts user to choose a time between 1-12 and then prompts
+  //user to determine if that is AM or PM. Updates will include structuring to read time inputs such as 11:30pm.
   let TaskTime = () => {
     let time = prompt('What time would you like your task completed? Enter a number 1-12. Substitute Colons for Periods')
-    if (time > 0 && time < 13) {
+    const minHourOfDay = 0;
+    const maxHourOfDay = 13;
+    if (time > minHourOfDay && time < maxHourOfDay) {
       let ofDay = prompt('AM or PM?')
       if (ofDay.toUpperCase() === 'AM' || ofDay.toUpperCase() === 'PM') {
         setTime(time +" " + ofDay)
@@ -62,7 +68,7 @@ export default function Card({ task }) {
             Date
           </button>
           <div id='date'>{newDate}</div>
-          <p>{Motivation[Math.floor(Math.random() * 11)]}</p>
+          <p>{Motivation[Math.floor(Math.random() * Motivation.length)]}</p>
         </div>
       </div>
     </div>
